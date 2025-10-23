7add2dbdbcdba19d1ddaecf4087eb8ab6030d6f8
YoussifMedhate <youssifmedhate35@gmail.com>
2025-10-24 00:54:30 +0300

    Implement infix expression validation function

--- Home-Work4/2-infix_validation.py
+++ Home-Work4/2-infix_validation.py
@@ -1,1 +1,26 @@
+def check_infix(expression: str) -> bool:
+    operatores = ('+', '-', '*', '/')
+    l = len(expression)
 
+    if not expression:
+        return False
+
+    if expression[0] in operatores or expression[-1] in operatores:
+        return False
+
+    for i in range(l - 1):
+        if expression[i] in operatores and expression[i + 1] in operatores:
+            return False
+
+        if (not expression[i].isdigit() and
+            expression[i] not in operatores and
+            expression[i] not in ('(', ')')):
+            return False
+
+    last = expression[-1]
+    if (not last.isdigit() and
+        last not in operatores and
+        last not in ('(', ')')):
+        return False
+
+    return True
