03c512d30331551af15a3ba5fa3feace4886a344
YoussifMedhate <youssifmedhate35@gmail.com>
2025-10-24 00:52:43 +0300

    Update 1-valid_parentheses.py

--- Home-Work4/1-valid_parentheses.py
+++ Home-Work4/1-valid_parentheses.py
@@ -1,1 +1,16 @@
+rom collections import deque
 
+def check_parentheses(expression: str) -> bool:
+    """Checks if all parentheses and brackets are balanced."""
+    
+    stk = deque()
+    pairs = {')': '(', ']': '[', '}': '{'} 
+    
+    for ch in expression:
+        if ch in pairs.values():
+            stk.append(ch)
+        elif ch in pairs:
+            if not stk or stk.pop() != pairs[ch]:
+                return False
+    
+    return not stk
